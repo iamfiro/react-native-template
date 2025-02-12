6e509570b905dfe88c4e8be3027b4f6291cc5cbd
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-12 21:22:25 +0900

    feat: add NavBar component and integrate it into HomeScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,38 +8,51 @@ import { Typo } from '@/components/atoms/Typo';
 import { TypoWeight } from '@/components/atoms/Typo/Typo.type';
 import { VStack } from '@/components/atoms/VStack';
 import { Icon } from '@/components/icon/glyph';
+import { NavBar } from '@/components/molecules/NavBar';
 import TitleHeader from '@/components/molecules/TitleHeader/TitleHeader';
 import { Text, SafeAreaView } from 'react-native';
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView style={{ paddingTop: 40 }}>
-            <TitleHeader showBackButton backButtonText="프로필">
-                안녕하세요
-            </TitleHeader>
-            <HStack style={{ padding: 20 }}>
-                <VStack>
-                    <Button variant={ButtonVariant.SUCCESS} onPress={() => {}}>
+        <>
+            <SafeAreaView style={{ paddingTop: 40 }}>
+                <TitleHeader showBackButton backButtonText="프로필">
+                    안녕하세요
+                </TitleHeader>
+                <HStack style={{ padding: 20 }}>
+                    <VStack>
+                        <Button
+                            variant={ButtonVariant.SUCCESS}
+                            onPress={() => {}}
+                        >
+                            <Text>저장s하기</Text>
+                        </Button>
+                        <Text style={{ fontFamily: 'PretendardBold' }}>
+                            저장s하기
+                        </Text>
                         <Text>저장s하기</Text>
-                    </Button>
-                    <Text style={{ fontFamily: 'PretendardBold' }}>
-                        저장s하기
-                    </Text>
-                    <Text>저장s하기</Text>
-                </VStack>
-                <Typo size={32} weight={TypoWeight.Bold}>
-                    아이이
-                </Typo>
-                <Badge variant={BadgeVariant.WARNING} size={BadgeSize.LARGE}>
-                    준비 안됨
-                </Badge>
-                <Label>앙기모찌</Label>
-                <Input
-                    placeholder="아이디"
-                    onChange={() => {}}
-                    leadingIcon={<Icon.asterisk />}
-                />
-            </HStack>
-        </SafeAreaView>
+                    </VStack>
+                    <Typo size={32} weight={TypoWeight.Bold}>
+                        아이이
+                    </Typo>
+                    <Badge
+                        variant={BadgeVariant.WARNING}
+                        size={BadgeSize.LARGE}
+                    >
+                        준비 안됨
+                    </Badge>
+                    <Label>앙기모찌</Label>
+                    <Input
+                        placeholder="아이디"
+                        onChange={() => {}}
+                        leadingIcon={<Icon.asterisk />}
+                    />
+                </HStack>
+            </SafeAreaView>
+            <NavBar>
+                <NavBar.Item icon={<Icon.asterisk />} selected screenName='..'>홈</NavBar.Item>
+                <NavBar.Item selected={false} screenName='..'>홈</NavBar.Item>
+            </NavBar>
+        </>
     );
 }
