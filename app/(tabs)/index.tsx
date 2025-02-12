79f3e86331e05c68aa91bcb20898ce418894ae18
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-12 20:31:21 +0900

    feat: implement TitleHeader component and update HomeScreen layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,42 @@
 import { Badge, BadgeSize, BadgeVariant } from '@/components/atoms/Badge';
 import { Button } from '@/components/atoms/Button';
-import {
-    ButtonSize,
-    ButtonVariant,
-} from '@/components/atoms/Button/index.type';
+import { ButtonVariant } from '@/components/atoms/Button/index.type';
+import { HStack } from '@/components/atoms/HStack';
 import { Input } from '@/components/atoms/Input';
 import { Label } from '@/components/atoms/Label';
 import { Typo } from '@/components/atoms/Typo';
 import { TypoWeight } from '@/components/atoms/Typo/index.type';
 import { VStack } from '@/components/atoms/VStack';
+import TitleHeader from '@/components/molecules/TitleHeader';
 import { Text, SafeAreaView } from 'react-native';
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView style={{ padding: 20 }}>
-            <VStack>
-                <Button variant={ButtonVariant.SUCCESS} onPress={() => {}}>
+        <SafeAreaView style={{ paddingTop: 40 }}>
+            <TitleHeader showBackButton backButtonText='프로필'>안녕하세요</TitleHeader>
+            <HStack style={{ padding: 20 }}>
+                <VStack>
+                    <Button variant={ButtonVariant.SUCCESS} onPress={() => {}}>
+                        <Text>저장s하기</Text>
+                    </Button>
+                    <Text style={{ fontFamily: 'PretendardBold' }}>
+                        저장s하기
+                    </Text>
                     <Text>저장s하기</Text>
-                </Button>
-                <Text style={{ fontFamily: 'PretendardBold' }}>저장s하기</Text>
-                <Text>저장s하기</Text>
-            </VStack>
-            <Typo size={32} weight={TypoWeight.Bold}>
-                아이이
-            </Typo>
-            <Badge variant={BadgeVariant.WARNING} size={BadgeSize.LARGE}>
-                준비 안됨
-            </Badge>
-            <Label>앙기모찌</Label>
-            <Input
-                placeholder="아이디"
-                onChange={() => {}}
-                leadingIcon={<Typo>asd</Typo>}
-            />
+                </VStack>
+                <Typo size={32} weight={TypoWeight.Bold}>
+                    아이이
+                </Typo>
+                <Badge variant={BadgeVariant.WARNING} size={BadgeSize.LARGE}>
+                    준비 안됨
+                </Badge>
+                <Label>앙기모찌</Label>
+                <Input
+                    placeholder="아이디"
+                    onChange={() => {}}
+                    leadingIcon={<Typo>asd</Typo>}
+                />
+            </HStack>
         </SafeAreaView>
     );
 }
---
+++ components/molecules/TitleHeader.tsx
@@ -0,0 +1,58 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { TitleHeaderProps } from '.';
+import { VStack } from '../atoms/VStack';
+import { Typo } from '../atoms/Typo';
+import { Icon } from '../icon/glyph';
+import { useTheme } from '@/hooks/useTheme';
+import { Color } from '@/constants/color';
+import { Box } from '../atoms/Box';
+import { useNavigation } from 'expo-router';
+import { useEffect, useState } from 'react';
+
+export default function TitleHeader({
+    children,
+    showBackButton,
+    backButtonText,
+    rightContent,
+}: TitleHeaderProps) {
+    const theme = useTheme();
+    const navigation = useNavigation();
+    const [chevronWidth, setChevronWidth] = useState(0);
+
+    useEffect(() => {
+        console.log(chevronWidth);
+    }, [chevronWidth]);
+
+    return (
+        <VStack
+            fullWidth
+            style={{
+                height: 46,
+                paddingHorizontal: 12,
+                backgroundColor: Color[theme].surface,
+            }}
+            justify={
+                !showBackButton && !rightContent ? 'center' : 'space-between'
+            }
+            align="center"
+        >
+            <VStack align='center' onLayout={(e) => setChevronWidth(e.nativeEvent.layout.width)}>
+                {showBackButton && (
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Icon.chevronLeft size={40} />
+                    </TouchableOpacity>
+                )}
+                {backButtonText && <Typo size={14} style={{marginLeft: -4}}>{backButtonText}</Typo>}
+            </VStack>
+            <Typo>{children}</Typo>
+            <Box
+                as='hoverable'
+                style={{
+                    minWidth: chevronWidth,
+                }}
+            >
+                {rightContent}
+            </Box>
+        </VStack>
+    );
+}
