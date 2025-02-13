85a82e87e9f887d616ce521f939f47b82c67080a
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-13 19:15:14 +0900

    feat: add Calender icon component and update index export

--- components/icon/glyph/index.ts
+++ components/icon/glyph/index.ts
@@ -1,7 +1,9 @@
 import Asterisk from "./asterisk";
+import Calender from "./calender";
 import ChevronLeft from "./chevron_left";
 
 export const Icon = {
     asterisk: Asterisk,
-    chevronLeft: ChevronLeft
+    chevronLeft: ChevronLeft,
+    calender: Calender,
 }
